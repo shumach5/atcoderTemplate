ed110e0d745eb8fe10dca42a1b2257f35783ce1c
shuhei <shumach5@gmail.com>
2020-06-14 23:28:52 +0900

    Add project files.

---
+++ AtCoder/ABCDEF/QuestionA.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtCoder.Abcdef
+{
+    public class QuestionA
+    {
+        public static void Main(string[] args)
+        {
+            var sw = new System.IO.StreamWriter(Console.OpenStandardOutput()) { AutoFlush = false };
+            Console.SetOut(sw);
+
+            //Input string
+            string s = Console.ReadLine();
+
+            //Input integer
+            long n = long.Parse(Console.ReadLine());
+
+            //Input string array
+            string[] inputStrArray = Console.ReadLine().Split(' ');
+
+            //Input int array
+            int[] inputLongArray = Console.ReadLine().Split(' ').Select(i => int.Parse(i)).ToArray();
+
+            //Write Code
+
+            Console.Out.Flush();
+        }
+    }
+}
---
+++ AtCoder/Program.cs
@@ -0,0 +1,21 @@
+﻿using AtCoder.Abcdef;
+
+namespace AtCoder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Run(args);
+        }
+
+        static void Run(string[] args) 
+        {
+            QuestionA.Main(args);
+            QuestionB.Main(args);
+            QuestionD.Main(args);
+            QuestionE.Main(args);
+            QuestionF.Main(args);
+        }
+    }
+}
