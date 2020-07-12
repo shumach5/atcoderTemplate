7f6dfc2d4f41f120fe0525d7f83a626958e2cd6c
shuhei <shumach5@gmail.com>
2020-07-12 23:07:26 +0900

    added input methods

---
+++ AtCoder/Libraries/Input.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AtCoder.Libraries
+{
+    public class Input
+    {
+        static public char[,] GetChar2d(int h, int w) 
+        {
+            char[,] c = new char[h, w];
+            for (int i = 0; i < h; i++)
+            {
+                var s = Console.ReadLine();
+                for (int j = 0; j < s.Length; j++)
+                {
+                    c[i, j] = s[j];
+                }
+            }
+
+            return c;
+        }
+
+        static public int[,] GetInt2d(int h, int w)
+        {
+            int[,] c = new int[h, w];
+            for (int i = 0; i < h; i++)
+            {
+                var s = Console.ReadLine();
+                for (int j = 0; j < s.Length; j++)
+                {
+                    c[i, j] = s[j];
+                }
+            }
+
+            return c;
+        }
+
+        static public long[,] GetLong2d(int h, int w)
+        {
+            long[,] c = new long[h, w];
+            for (int i = 0; i < h; i++)
+            {
+                var s = Console.ReadLine();
+                for (int j = 0; j < s.Length; j++)
+                {
+                    c[i, j] = s[j];
+                }
+            }
+
+            return c;
+        }
+    }
+}
