956301bcfc9565ed41f14834e6800a3e12db5f11
shuhei <shumach5@gmail.com>
2020-12-18 17:44:49 +0900

    Added permutation and subsets

---
+++ AtCoder/Libraries/BackTracking.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace AtCoder.Libraries
+{
+    public class BackTracking
+    {
+		public IList<IList<int>> GetSubsets(int[] nums)
+		{
+			for (int i = 0; i <= nums.Length; i++)
+			{
+				SubSetsRecursive(nums, new List<int>(), 0, i);
+			}
+
+			return _output;
+		}
+
+		private void SubSetsRecursive(int[] nums, List<int> temp, int first, int num)
+		{
+			if (temp.Count == num)
+			{
+				_output.Add(temp.ToList());
+				return;
+			}
+
+			for (int i = first; i < nums.Length; i++)
+			{
+				temp.Add(nums[i]);
+				SubSetsRecursive(nums, temp, i + 1, num);
+				temp.RemoveAt(temp.Count - 1);
+			}
+		}
+
+		public IList<IList<int>> GetPermute(int[] nums)
+		{
+			PermuteRecursive(nums, 0);
+			return _output;
+		}
+
+		void PermuteRecursive(int[] nums, int first)
+		{
+			if (first == nums.Length)
+			{
+				_output.Add(nums.ToList());
+			}
+
+			for (int i = first; i < nums.Length; i++)
+			{
+				(nums[first], nums[i]) = (nums[i], nums[first]);
+				PermuteRecursive(nums, first + 1);
+				(nums[first], nums[i]) = (nums[i], nums[first]);
+			}
+		}
+
+		public IList<IList<int>> _output = new List<IList<int>>();
+	}
+}
